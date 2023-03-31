458f773c61be0d4f90d59b77b433faad48984b76
Alan-day <lesterthenovice@gmail.com>
2023-03-31 17:24:50 +0100

    javascrip handlers

---
+++ app.js
@@ -0,0 +1,40 @@
+const clearedItems = document.querySelector(".clear");
+let userInput = document.querySelector("#numberInput");
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+
+let firstPart = "";
+let operator = "";
+let secondPart = "";
+
+const handleNumberClick = (event) => {
+  if (operator != "") {
+    secondPart = event.target.innerText;
+  } else if (operator == "") {
+    firstPart = event.target.innerText;
+  }
+};
+
+numbers.forEach((element) => {
+  element.addEventListener("click", handleNumberClick);
+});
+
+const handleOperationClick = (event) => {
+  operator = event.target.innerText;
+  console.log(event.target.innerText);
+};
+
+operations.forEach((element) => {
+  element.addEventListener("click", handleOperationClick);
+});
+
+/*const handleClearFunction = (event) => {
+  firstPart = "";
+  operator = "";
+  partTwo = "";
+};
+
+clearedItems.forEach((element) => {
+  element.addEventListener("click", handleClearFunction);
+});
+*/
