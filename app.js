4d64b82754799e0996d5bec1338a9e30079b53c7
Alan-day <lesterthenovice@gmail.com>
2023-04-03 16:59:08 +0100

    porvided calculation functions

--- app.js
+++ app.js
@@ -2,37 +2,91 @@ const clearedItems = document.querySelector(".clear");
 let userInput = document.querySelector("#numberInput");
 const numbers = document.querySelectorAll(".number");
 const operations = document.querySelectorAll(".operation");
+const equals = document.querySelector("#equals");
 
-let firstPart = "";
-let operator = "";
-let secondPart = "";
+let firstPart = " ";
+let operator = " ";
+let secondPart = " ";
+
+/*const calculate=(numbers)=> {
+const value = numbers.textContent
+calculation.push(value)
+totalCalculation =calculate.join('')
+userInput.textContent = totalCalculation
+console.log(ca);
+}
+*/
 
 const handleNumberClick = (event) => {
+  if (operator == " ") {
+    userInput.innerHTML += event.target.innerText;
+    firstPart += event.target.innerText;
+    console.log(firstPart);
+  } else if (operator != " ") {
+    userInput.innerHTML += event.target.innerText;
+    secondPart += event.target.innerText;
+    console.log(secondPart);
+  } else {
+  }
+};
+
+const handleEquals = (event) => {
+  if (operator == "+") {
+    userInput.innerHTML = parseInt(firstPart) + parseInt(secondPart);
+    console.log(userInput.innerHTML);
+  } else if (operator == "-") {
+    userInput.innerHTML = parseInt(firstPart) - parseInt(secondPart);
+    console.log(userInput.innerHTML);
+  } else if (operator == "÷") {
+    userInput.innerHTML = parseInt(firstPart) / parseInt(secondPart);
+
+    console.log(userInput.innerHTML);
+  } else if (operator == "x") {
+    userInput.innerHTML = parseInt(firstPart) * parseInt(secondPart);
+
+    console.log(userInput.innerHTML);
+  } else if (operator == "%") {
+    userInput.innerHTML = parseInt(firstPart) * 0.01 * parseInt(secondPart);
+    console.log(userInput.innerHTML);
+  }
+};
+
+/*
+  
+  
+let calculation = [];
+let totalCalculation;
+
+
+  
+  
   if (operator != "") {
     secondPart = event.target.innerText;
   } else if (operator == "") {
     firstPart = event.target.innerText;
-  }
-};
+  }*/
+
 numbers.forEach((element) => {
   element.addEventListener("click", handleNumberClick);
 });
 
 const handleOperationClick = (event) => {
+  userInput.innerHTML += event.target.innerText;
   operator = event.target.innerText;
-  console.log(event.target.innerText);
+  console.log(operator);
 };
 
 operations.forEach((element) => {
   element.addEventListener("click", handleOperationClick);
 });
 
+equals.addEventListener("click", handleEquals);
+
 const handleClearFunction = (event) => {
-  event.target.firstPart = "";
-  event.target.operator = "";
-  event.target.partTwo = "";
+  userInput.innerHTML += event.target.innerText;
+  firstPart = "";
+  operator = "";
+  partTwo = "";
 };
 
-clearedItems.forEach((element) => {
-  element.addEventListener("click", handleClearFunction);
-});
+equals.addEventListener("click", handleClearFunction);
