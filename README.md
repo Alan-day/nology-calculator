b4dfd47680d46d66279a153139001583eee26e79
Alan <lesterthenovice@gmail.com>
2023-07-20 19:05:23 +0100

    Dot and change sign fixed. Readme updated

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # nology-calculator
 
-A simple calculator on a basic webpage
-developed in vanilla JavaScript
+Welcome to my calculator app!
 
-it takes maximum of 2 inputs of infinite amount of digits
-The change-sign, dot and don't work yet but I aim to solve this issue
+A simple calculator running on a basic page developed in vanilla JavaScript.
 
-Alan Szafarczyk 04/04/2023
+It takes maximum of 2 inputs of infinite amount of digits.
+
+The change-sign and dot buttons work now but my method of adding decimals limits me to adding only one number with a decimal to the equation. At the moment in suggest to press "=" sign after adding decimals so to perform a basic equation (like 12.3 + 12.3) press buttons in that order: 12 "." 3 "=" "+" 12 "." 3 "="
+
+"%" button calculates percentage of a number so 5%100 will return 5 and 1%1000 will return 10.
+
+Bugs:
+
+I can't process two negative numbers. If the first one is negative the whole calculation returns an error. However when the first number is positive everything works fine.
+
+Alan
+20/07/2023
--- app.js
+++ app.js
@@ -11,74 +11,91 @@ let firstPart = " ";
 let operator = " ";
 let secondPart = " ";
 let product = " ";
+let isDecimal = false;
 
 const handleNumberClick = (event) => {
-  if (operator == " ") {
+  if (operator === " ") {
     userInput.innerHTML += event.target.innerText;
     firstPart += event.target.innerText;
-    console.log(firstPart);
-  } else if (operator != " ") {
+  } else {
     userInput.innerHTML += event.target.innerText;
     secondPart += event.target.innerText;
-    console.log(secondPart);
-  } else {
   }
 };
-// equal function
+
 const handleEquals = (event) => {
-  if (operator == "+") {
-    userInput.innerHTML = parseInt(firstPart) + parseInt(secondPart);
-
-    console.log(userInput.innerHTML);
-  } else if (operator == "-") {
-    userInput.innerHTML = parseInt(firstPart) - parseInt(secondPart);
-  } else if (operator == "÷") {
-    userInput.innerHTML = parseInt(firstPart) / parseInt(secondPart);
-  } else if (operator == "x") {
-    userInput.innerHTML = parseInt(firstPart) * parseInt(secondPart);
-  } else if (operator == "%") {
-    userInput.innerHTML = parseInt(firstPart) * 0.01 * parseInt(secondPart);
-  } else {
+  if (operator === "+") {
+    userInput.innerHTML = parseFloat(firstPart) + parseFloat(secondPart);
+  } else if (operator === "-") {
+    userInput.innerHTML = parseFloat(firstPart) - parseFloat(secondPart);
+  } else if (operator === "÷") {
+    userInput.innerHTML = parseFloat(firstPart) / parseFloat(secondPart);
+  } else if (operator === "x") {
+    userInput.innerHTML = parseFloat(firstPart) * parseFloat(secondPart);
+  } else if (operator === "%") {
+    userInput.innerHTML = parseFloat(firstPart) * 0.01 * parseFloat(secondPart);
   }
+
+  // Reset calculator state after calculation
+  firstPart = userInput.innerHTML;
+  operator = " ";
+  secondPart = "";
+  isDecimal = false;
 };
-equals.addEventListener("click", handleEquals);
 
-numbers.forEach((element) => {
-  element.addEventListener("click", handleNumberClick);
-});
-
-//operation clicking
 const handleOperationClick = (event) => {
   userInput.innerHTML += event.target.innerText;
   operator = event.target.innerText;
-  console.log(operator);
 };
 
-operations.forEach((element) => {
-  element.addEventListener("click", handleOperationClick);
-});
-
-//clear function
 const handleClearFunction = (event) => {
   userInput.innerHTML = null;
   firstPart = " ";
   operator = " ";
   secondPart = " ";
+  isDecimal = false;
 };
 
+const handleSign = (event) => {
+  if (operator === " ") {
+    if (firstPart.includes(".")) {
+      firstPart = (-1 * parseFloat(firstPart)).toString();
+    } else {
+      firstPart = (-1 * parseInt(firstPart)).toString();
+    }
+    userInput.innerHTML = firstPart;
+  } else {
+    if (secondPart.includes(".")) {
+      secondPart = (-1 * parseFloat(secondPart)).toString();
+    } else {
+      secondPart = (-1 * parseInt(secondPart)).toString();
+    }
+    userInput.innerHTML = firstPart + operator + secondPart;
+  }
+};
+
+const handleDotFunction = (event) => {
+  if (!isDecimal) {
+    userInput.innerHTML += event.target.innerText;
+    if (operator === " ") {
+      firstPart += event.target.innerText;
+    } else {
+      secondPart += event.target.innerText;
+    }
+    isDecimal = true;
+  }
+};
+
+// Event listeners
+numbers.forEach((element) => {
+  element.addEventListener("click", handleNumberClick);
+});
+
+operations.forEach((element) => {
+  element.addEventListener("click", handleOperationClick);
+});
+
+equals.addEventListener("click", handleEquals);
 clear.addEventListener("click", handleClearFunction);
-
-/* const handleSignFunction = (event) => {
- if(secondPart == " ") {
-    userInput.innerHTML == firstPart.concat(".")  
-  } else if (firstPart != " "){
-  userInput.innerHTML == secondPart.concat(".")  
-}};
-
-
-sign.addEventListener("click", handleSignFunction);
+sign.addEventListener("click", handleSign);
 dot.addEventListener("click", handleDotFunction); 
-
-
-
-*/
